33b56b034011cd93951444aa6e661c51f80c6e6c
jupbc <jupbcan100@hotmail.com>
2024-12-27 16:54:42 -0500

    change: Vegetation gallery title added to images

--- src/pages/VegetationGallery.js
+++ src/pages/VegetationGallery.js
@@ -32,18 +32,21 @@ const VegetationGallery = () => {
             {t('gallery.title')}
         </h2>
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-4 gap-6 mt-8 mx-auto">
-            {imageUrls.map((image) => (
-                <div
-                    key={image.fileName}
-                    className="flex items-center justify-center overflow-hidden w-[300px] h-[300px] mx-auto"
-                >
-                    <img
-                        src={image.url}
-                        alt={image.fileName}
-                        className="object-fit items-center justify-center mix-blend-multiply"
-                    />
-                </div>
-            ))}
+        {imageUrls.map((image) => (
+            <div
+            key={image.fileName}
+            className="relative flex items-center justify-center overflow-hidden w-[300px] h-[300px] mx-auto group"
+            >
+            <img
+                src={image.url}
+                alt={image.fileName}
+                className={`w-full h-full object-cover rounded-lg transition-opacity`}
+            />
+            <div className="absolute inset-0 flex items-center justify-center bg-[#0C1811] bg-opacity-70 opacity-0 group-hover:opacity-100 transition-opacity duration-300 rounded-lg">
+                <p className="text-[#F9FBFA] text-xl font-bold">{image.fileName.replace(/\.[^/.]+$/, '').replace(/_/g, ' ')}</p>
+            </div>
+            </div>
+        ))}
         </div>
     </>
     );
